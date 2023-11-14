0d0268e877b9b6bf3a010313dd73726cc021ad18
yodfhafx <yodfha2014@gmail.com>
2023-11-14 22:08:48 +0700

    update shopping cart and backend authen

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,29 @@
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.route("/logout").post(logoutUser);
+router.route("/auth").post(authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser)
+  .delete(protect, admin, deleteUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,38 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
+connectDB(); // connect mongodb
+
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
